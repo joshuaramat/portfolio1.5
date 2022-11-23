262fc98b25147a674a95927065ededdbcd577bdd
Joshua Ramat <ramatjoshua@gmail.com>
2022-11-22 18:41:40 -0800

    set up steps for blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# Create a Blog model
+  """
+    title
+    publish date (pub_date)
+    body
+    image
+  """
+# Add the blog app to the settings
+# create migration
+# migrate
+# add to the admin
