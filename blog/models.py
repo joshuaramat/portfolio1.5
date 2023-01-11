c7ed32e56879bac77cc6e4be9e23d57162155575
Joshua Ramat <ramatjoshua@gmail.com>
2023-01-10 16:43:26 -0800

    create migrations for blog model

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
-# Create your models here.
-# Create a Blog model
-  """
-    title
-    publish date (pub_date)
-    body
-    image
-  """
-# Add the blog app to the settings
-# create migration
-# migrate
-# add to the admin
+class Blog(models.Model):
+  title = models.CharField(max_length=250)
+  pub_date = models.DateTimeField()
+  body = models.TextField()
+  image = models.ImageField(upload_to='images/')
